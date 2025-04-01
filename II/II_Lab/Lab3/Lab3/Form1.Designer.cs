5e9a6fd375b912984242a6926e4a9cef5c58c51f
rares478 <rares478@gmail.com>
2025-04-01 22:01:50 +0300

    II
    Lab 3 full

--- II/II_Lab/Lab3/Lab3/Form1.Designer.cs
+++ II/II_Lab/Lab3/Lab3/Form1.Designer.cs
@@ -29,12 +29,252 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.database1DataSet = new Lab3.Database1DataSet();
+            this.facultatiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.facultatiTableAdapter = new Lab3.Database1DataSetTableAdapters.FacultatiTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameFacDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUniName = new System.Windows.Forms.TextBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btDeleteFaculta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facultatiBindingSource)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.codeDataGridViewTextBoxColumn,
+            this.nameFacDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.facultatiBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(645, 211);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // facultatiBindingSource
+            // 
+            this.facultatiBindingSource.DataMember = "Facultati";
+            this.facultatiBindingSource.DataSource = this.database1DataSet;
+            // 
+            // facultatiTableAdapter
+            // 
+            this.facultatiTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // codeDataGridViewTextBoxColumn
+            // 
+            this.codeDataGridViewTextBoxColumn.DataPropertyName = "Code";
+            this.codeDataGridViewTextBoxColumn.HeaderText = "Code";
+            this.codeDataGridViewTextBoxColumn.Name = "codeDataGridViewTextBoxColumn";
+            // 
+            // nameFacDataGridViewTextBoxColumn
+            // 
+            this.nameFacDataGridViewTextBoxColumn.DataPropertyName = "NameFac";
+            this.nameFacDataGridViewTextBoxColumn.HeaderText = "NameFac";
+            this.nameFacDataGridViewTextBoxColumn.Name = "nameFacDataGridViewTextBoxColumn";
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(211, 25);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(264, 20);
+            this.tbCity.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "City";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "University Name";
+            // 
+            // tbUniName
+            // 
+            this.tbUniName.Location = new System.Drawing.Point(211, 66);
+            this.tbUniName.Name = "tbUniName";
+            this.tbUniName.Size = new System.Drawing.Size(264, 20);
+            this.tbUniName.TabIndex = 4;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(481, 26);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 5;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(481, 63);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 6;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(481, 105);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdate.TabIndex = 7;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Code";
+            // 
+            // tbCode
+            // 
+            this.tbCode.Location = new System.Drawing.Point(211, 103);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(264, 20);
+            this.tbCode.TabIndex = 9;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btAdd);
+            this.tabPage1.Controls.Add(this.tbUniName);
+            this.tabPage1.Controls.Add(this.btUpdate);
+            this.tabPage1.Controls.Add(this.tbCode);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.tbCity);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.btDelete);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btDeleteFaculta);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btDeleteFaculta
+            // 
+            this.btDeleteFaculta.Location = new System.Drawing.Point(241, 262);
+            this.btDeleteFaculta.Name = "btDeleteFaculta";
+            this.btDeleteFaculta.Size = new System.Drawing.Size(75, 23);
+            this.btDeleteFaculta.TabIndex = 1;
+            this.btDeleteFaculta.Text = "Delete";
+            this.btDeleteFaculta.UseVisualStyleBackColor = true;
+            this.btDeleteFaculta.Click += new System.EventHandler(this.btDeleteFaculta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facultatiBindingSource)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource facultatiBindingSource;
+        private Database1DataSetTableAdapters.FacultatiTableAdapter facultatiTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameFacDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUniName;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btDeleteFaculta;
     }
 }
 
--- II/II_Lab/Lab3/Lab3/Form1.cs
+++ II/II_Lab/Lab3/Lab3/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,104 @@ namespace Lab3
 {
     public partial class Form1 : Form
     {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\Faculta\II\II_Lab\Lab3\Lab3\Database1.mdf;Integrated Security=True";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Facultati' table. You can move, or remove it, as needed.
+            this.facultatiTableAdapter.Fill(this.database1DataSet.Facultati);
+
+        }
+
+        private void AddUniversity(string nameUniv, string city, int code)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Universitati (NameUniv, City, Code) VALUES (@NameUniv, @City, @Code)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NameUniv", nameUniv);
+                    command.Parameters.AddWithValue("@City", city);
+                    command.Parameters.AddWithValue("@Code", code);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private void RemoveUniversity(int code)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Universitati WHERE Code = @Code";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Code", code);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void ModifyUniversity(string nameUniv, string city, int code)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Universitati SET NameUniv = @NameUniv, City = @City WHERE Code = @Code";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@NameUniv", nameUniv);
+                    command.Parameters.AddWithValue("@City", city);
+                    command.Parameters.AddWithValue("@Code", code);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            AddUniversity(tbUniName.Text, tbCity.Text, Int32.Parse(tbCode.Text));
+        }
+
+        private void btUpdate_Click(object sender, EventArgs e)
+        {
+            ModifyUniversity(tbUniName.Text, tbCity.Text, Int32.Parse(tbCode.Text));
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            RemoveUniversity(Int32.Parse(tbCode.Text));
+        }
+
+        private void btDeleteFaculta_Click(object sender, EventArgs e)
+        {
+            int selectedRowIndex = dataGridView1.CurrentCell.RowIndex;
+            int code = (int)database1DataSet.Facultati.Rows[selectedRowIndex]["Code"];
+
+            database1DataSet.Facultati.Rows[selectedRowIndex].Delete();
+            facultatiTableAdapter.Update(database1DataSet.Facultati);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Facultati WHERE Code = @Code";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Code", code);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
